9be0e9157113adc0d0627bb03c1c6b3690dc5bcc
shriram11k <shriram11k@outlook.com>
2025-08-19 19:03:26 +0530

    Added resource group configuration

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -1,15 +1,9 @@
 # Terraform module: resource_group
-# Replace with actual resource definitions
-
-terraform {
-  required_version = ">= 1.3.0"
+resource "azurerm_resource_group" "this" {
+  name     = var.name
+  location = var.location
 }
 
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "resource_group_rg" {
-  name     = "resource_group-rg"
-  location = "East US"
+output "name" {
+  value = azurerm_resource_group.this.name
 }
